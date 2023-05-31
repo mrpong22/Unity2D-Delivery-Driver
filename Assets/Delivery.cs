fc106863934bcd35b44f2865b1717d34c98f4ed9
mrpong22 <dxphong2000@gmail.com>
2023-05-31 11:34:21 +0700

    Project

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -2,15 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Collision : MonoBehaviour
+public class Delivery : MonoBehaviour
 {
     void OnCollisionEnter2D(Collision2D collision)
     {
         Debug.Log("Do not destroy the car");
     }
 
-    void OnTriggerEnter2D(Collider2D collision)
+    void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Passed through the line");    
+        if (other.tag == "Package")
+        {
+            Debug.Log("Package picked up");
+        }
     }
 }
